4f403eafa2ffd5326c27e5283d068606ab9f6ec8
cat <fayfoxcat@gmail.com>
2022-08-11 14:51:18 +0800

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
 <template>
-    <div>{{ color }}</div>
-    <button @click="color = 'red'">红色</button>
-    <button @click="color = 'yellow'">黄色</button>
-    <button @click="color = 'green'">绿色</button>
-    <Son />
+  <div>{{ color }}</div>
+  <button @click="color = 'red'">红色</button>
+  <button @click="color = 'yellow'">黄色</button>
+  <button @click="color = 'green'">绿色</button>
+  <button @click="update('456')">app按钮</button>
+  <Son/>
 </template>
 
 <script lang="ts">
-import { defineComponent, provide, ref } from 'vue'
+import {defineComponent, provide, ref} from 'vue'
 import Son from '@/components/Son.vue'
 
 export default defineComponent({
-    name: 'App',
-    component: {
-        Son
-    },
-    setup() {
-        const color = ref('red')
-        provide('color', color)
-        return {
-            color,
-        }
+  name: 'App',
+  components: {
+    Son
+  },
+  setup() {
+    const color = ref('red')
+    const update = ((value: string) => {
+      console.log("abc"+value)
+    })
+    provide('color', color)
+    provide('update', update)
+    return {
+      color,
+      update
     }
+  }
 })
 </script>
 
--- src/components/Son.vue
+++ src/components/Son.vue
@@ -1,18 +1,23 @@
 <template>
-    <h3 :style="{color}">Son组件</h3>
+  <h3>hello</h3>
+  <h3 :style="{color}">Son组件</h3>
+  <button @click="update()">按钮</button>
 </template>
 
 <script lang="ts">
-import { defineComponent, inject } from 'vue'
+import {defineComponent, inject} from 'vue'
 
 export default defineComponent({
-    name: 'Son',
-    setup () {
-        const color = inject('color')
-        return {
-            color,
-        }
+  name: 'Son',
+  setup() {
+    const color = inject('color')
+    const value = 123
+    const update = inject('update', value)
+    return {
+      color,
+      update
     }
+  }
 })
 </script>
 

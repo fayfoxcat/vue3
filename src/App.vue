380ac404c892b60135a53fff9e5ad51db10fcc3c
cat <fayfoxcat@gmail.com>
2022-08-11 14:53:29 +0800

    toList小结

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,16 @@
 <template>
-  <div>{{ color }}</div>
-  <button @click="color = 'red'">红色</button>
-  <button @click="color = 'yellow'">黄色</button>
-  <button @click="color = 'green'">绿色</button>
-  <button @click="update('456')">app按钮</button>
-  <Son/>
+  <div></div>
 </template>
 
 <script lang="ts">
-import {defineComponent, provide, ref} from 'vue'
-import Son from '@/components/Son.vue'
+import {defineComponent} from 'vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    Son
-  },
   setup() {
-    const color = ref('red')
-    const update = ((value: string) => {
-      console.log("abc"+value)
-    })
-    provide('color', color)
-    provide('update', update)
+
     return {
-      color,
-      update
+
     }
   }
 })
